525aa28b228f579eb746dc24721c55cdc91e7475
Dmitry Pavluk <dmitry@dmitrypavluk.com>
2022-08-23 18:08:42 -0400

    refresh site for a darker look

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,47 +1,47 @@
-import React from 'react';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styled from 'styled-components';
+import React from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
-const Fade = require('react-reveal/Fade');
+const Fade = require("react-reveal/Fade");
 
 interface Props {
-	time: string;
-	title: string;
-	icon: IconProp;
-	left?: string;
+  time: string;
+  title: string;
+  icon: IconProp;
+  left?: string;
 }
 
 export class Timeline extends React.Component<Props> {
-	render() {
-		const props = this.props;
+  render() {
+    const props = this.props;
 
-		const IconBox = styled.div({
-			background: '#fff',
-			fontSize: '24px',
-			color: '#25d2db',
-			position: 'absolute',
-			left: this.props.left ? this.props.left : '-10px',
-			top: 0,
-			zIndex: 1,
-			fontWeight: 900
-		});
+    const IconBox = styled.div({
+      background: "#09091a",
+      fontSize: "24px",
+      color: "#25d2db",
+      position: "absolute",
+      left: this.props.left ? this.props.left : "-10px",
+      top: 0,
+      zIndex: 1,
+      fontWeight: 900
+    });
 
-		return (
-			<div className="timeline-container">
-				<Fade bottom>
-					<div className="content">
-						<span className="time">{this.props.time}</span>
-						<h3 className="title">{this.props.title}</h3>
-						{this.props.children}
-					</div>
-				</Fade>
-				<IconBox>
-					<FontAwesomeIcon icon={props.icon} />
-				</IconBox>
-			</div>
-		);
-	}
+    return (
+      <div className="timeline-container">
+        <Fade bottom>
+          <div className="content">
+            <span className="time">{this.props.time}</span>
+            <h3 className="title">{this.props.title}</h3>
+            {this.props.children}
+          </div>
+        </Fade>
+        <IconBox>
+          <FontAwesomeIcon icon={props.icon} />
+        </IconBox>
+      </div>
+    );
+  }
 }
 
 export default Timeline;
--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import { Section, Timeline } from '../components'
-import history from '../data/history.json'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { Section, Timeline } from "../components";
+import history from "../data/history.json";
 
-const career = history.career
+const career = history.career;
 
 export class Experience extends React.Component {
-  render () {
-
+  render() {
     return (
       <Section id="experience" title="Experience">
         <Row>
           <Col md={12}>
             <div className="timeline edu bg-white rounded shadow-dark padding-30 overflow-hidden">
-
-              { career.map(c => (
-                <Timeline key={c.company} time={c.period} title={c.title} icon={['fas', 'briefcase']} left="-7px">
+              {career.map((c) => (
+                <Timeline
+                  key={c.company}
+                  time={c.period}
+                  title={c.title}
+                  icon={["fas", "briefcase"]}
+                  left="-7px"
+                >
                   <p>{c.company}</p>
-                  <div dangerouslySetInnerHTML={ { __html: c.description } } />
+                  <div dangerouslySetInnerHTML={{ __html: c.description }} />
                 </Timeline>
               ))}
 
@@ -26,8 +30,8 @@ export class Experience extends React.Component {
           </Col>
         </Row>
       </Section>
-    )
+    );
   }
 }
 
-export default Experience
+export default Experience;
